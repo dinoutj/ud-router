76293b6e869c2d2573c4598fd70ebfb8aae74f03
Dino <dinoutj@hotmail.com>
2018-03-01 22:18:17 +0100

    Nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import './Blog.css';
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
-  render() {
-    return (
-      <div className="Blog">
-        <header>
-          <nav>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
-            </ul>
-          </nav>
-        </header>
-        <Route path="/" render={() => <h1>Home</h1>} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash:'#submit',
+                                search: '?quick-submit-true'
+                            }}>New Post</NavLink></li>
+
+                        </ul>
+                    </nav>
+                </header>
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default Blog;
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -10,6 +10,10 @@ class NewPost extends Component {
         author: 'Dino'
     }
 
+    componentDidMount() {
+        console.log(this.props);
+    }
+
     postDataHandler = () => {
       const data = {
         title: this.state.title,
