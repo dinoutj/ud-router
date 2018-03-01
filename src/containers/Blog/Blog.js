ca619a9f62a825f8fd957b6c9941ed665bd0e4e9
Dino Utjesinovic <dino@ri-pos.hr>
2018-03-01 15:52:25 +0100

    Router intro

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+
+import Posts from './Posts/Posts';
+import './Blog.css';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/new-post">New Post</a></li>
+            </ul>
+          </nav>
+        </header>
+        <Route path="/" render={() => <h1>Home</h1>} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
